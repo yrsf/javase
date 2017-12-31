160c8f2ee20234c1832d4206b79e9ec99a7c4f3a
antony <haozyc@sina.com>
2018-01-01 03:09:45 +0800

    init

---
+++ threadpool/src/main/java/yrsf.javase.threadpool/PausableThreadPoolExecutor.java
@@ -0,0 +1,72 @@
+package yrsf.javase.threadpool;
+
+import java.util.concurrent.*;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by Antony on 7/26/17.
+ */
+public class PausableThreadPoolExecutor extends ThreadPoolExecutor {
+
+    private boolean isPaused;
+
+    private Lock pauseLock = new ReentrantLock();
+
+    private Condition pauseCondition = pauseLock.newCondition();
+
+
+    public PausableThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
+    }
+
+    public PausableThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, ThreadFactory threadFactory) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory);
+    }
+
+    public PausableThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, RejectedExecutionHandler handler) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, handler);
+    }
+
+    public PausableThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, ThreadFactory threadFactory, RejectedExecutionHandler handler) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory, handler);
+    }
+
+    @Override
+    protected void beforeExecute(Thread t, Runnable r) {
+        super.beforeExecute(t, r);
+
+        pauseLock.lock();
+
+        try {
+            while (isPaused) {
+                pauseCondition.await();
+            }
+        } catch (InterruptedException e) {
+            t.interrupt();
+        } finally {
+            pauseLock.unlock();
+        }
+
+    }
+
+    public void pause() {
+        pauseLock.lock();
+        try {
+            isPaused = true;
+        } finally {
+            pauseLock.unlock();
+        }
+    }
+
+    public void resume() {
+        pauseLock.lock();
+        try {
+            isPaused = false;
+            pauseCondition.signalAll();
+        } finally {
+            pauseLock.unlock();
+        }
+    }
+}
---
+++ threadpool/src/main/java/yrsf.javase.threadpool/TraceThreadPoolExecutor.java
@@ -0,0 +1,48 @@
+package yrsf.javase.threadpool;
+
+import java.util.concurrent.*;
+
+/**
+ * Created by Antony on 7/26/17.
+ */
+public class TraceThreadPoolExecutor extends ThreadPoolExecutor {
+
+    public TraceThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
+    }
+
+    public TraceThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, ThreadFactory threadFactory) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory);
+    }
+
+    public TraceThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, RejectedExecutionHandler handler) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, handler);
+    }
+
+    public TraceThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, ThreadFactory threadFactory, RejectedExecutionHandler handler) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory, handler);
+    }
+
+    @Override
+    public void execute(Runnable command) {
+        super.execute(wrap(command, clientTrace()));
+    }
+
+    private Exception clientTrace() {
+        return new Exception("Client stack trace." + Thread.currentThread().getName());
+    }
+
+    private Runnable wrap(final Runnable task, final Exception clientStack) {
+        return new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    task.run();
+                } catch (Exception e) {
+                    clientStack.printStackTrace();
+                    throw e;
+                }
+            }
+        };
+    }
+}
